195782f736cc7d30cb98e734fe3d898b59bec840
User <User@DESKTOP-CHKLJ5S>
2023-09-07 21:50:56 +0300

    new

--- Hillel_course/Lesson_3/days_of_sunday/Program.cs
+++ Hillel_course/Lesson_3/days_of_sunday/Program.cs
@@ -11,27 +11,27 @@
             {
                 Console.WriteLine("Понеділок");
             }
-            if (x == 2)
+            else if (x == 2)
             {
                 Console.WriteLine("Вівторок");
             }
-            if (x == 3)
+            else if (x == 3)
             {
                 Console.WriteLine("Середа");
             }
-            if (x == 4)
+            else if (x == 4)
             {
                 Console.WriteLine("Четверг");
             }
-            if (x == 5)
+            else if (x == 5)
             {
                 Console.WriteLine("П'ятниця");
             }
-            if (x == 6)
+            else if (x == 6)
             {
                 Console.WriteLine("Суббота");
             }
-            if (x == 7)
+            else if (x == 7)
             {
                 Console.WriteLine("Неділя");
             }
