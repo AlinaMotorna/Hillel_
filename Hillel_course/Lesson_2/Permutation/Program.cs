bd23fb4c05242739aff4479a0ad4b51098218786
User <User@DESKTOP-CHKLJ5S>
2023-09-07 18:24:03 +0300

    new

---
+++ Hillel_course/Lesson_2/Permutation/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first_number1 = 1;
+            int second_number2 = 2;
+            int third_number3 = 3;
+
+            Console.WriteLine(third_number3);
+            Console.WriteLine(second_number2);
+            Console.WriteLine(first_number1);
+            Console.ReadKey();
+        }
+    }
+}
