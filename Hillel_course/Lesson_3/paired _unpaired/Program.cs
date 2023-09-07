fa6e48bbd747d43c0324b960e278fe8a101380fd
User <User@DESKTOP-CHKLJ5S>
2023-09-07 20:01:25 +0300

    new leasson

--- Hillel_course/Lesson_3/paired _unpaired/Program.cs
+++ Hillel_course/Lesson_3/paired _unpaired/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int x = Convert.(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
             if ((x % 2) == 0) 
             {
                 Console.WriteLine("парне");
