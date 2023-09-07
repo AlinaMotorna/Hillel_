fd1fb143df1b4349d614992510891b1fff3ee551
User <User@DESKTOP-CHKLJ5S>
2023-09-07 18:28:46 +0300

    news

---
+++ Hillel_course/Lesson_2/reverse_order/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace reverse_order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first_number = 1;
+            int second_number = 2;
+            int third_number = 3;
+
+            Console.WriteLine(third_number);
+            Console.WriteLine(second_number);
+            Console.WriteLine(first_number);
+            Console.ReadKey();
+        }
+    }
+}
