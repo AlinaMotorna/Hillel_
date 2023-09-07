51a27f8196f86dd0eef31e35a6b67f981e103a10
User <User@DESKTOP-CHKLJ5S>
2023-09-07 20:00:27 +0300

    new

--- Hillel_course/Lesson_2/the_square_of_a_number/Program.cs
+++ Hillel_course/Lesson_2/the_square_of_a_number/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace the_square_of_a_number
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter sum: ");
+            int inputNum = int.Parse(Console.ReadLine());
+
+            int firstDigit = inputNum;
+            int secondDigit = inputNum;
+
+            Console.WriteLine($"{firstDigit * secondDigit}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hillel_course/Lesson_3/paired _unpaired/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace paired_unpired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.(Console.ReadLine());
+            if ((x % 2) == 0) 
+            {
+                Console.WriteLine("парне");
+                    }
+            else
+            {
+                Console.WriteLine("не парне");
+                    }
+            Console.ReadKey();
+        }
+    }
+}
