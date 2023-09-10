655059b40a658d88a0e5f9a277d9a444870f959c
User <User@DESKTOP-CHKLJ5S>
2023-09-10 21:37:14 +0300

    new

---
+++ Hillel_course/Lesson_4/Random/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Rando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int a = 0, num = 0;
+                Random random = new Random();
+                num = random.Next(1, 100);
+                do
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    if (num == a)
+                    {
+                        Console.WriteLine("Правильно!");
+                        break;
+                    }
+                    else if (a < num)
+                    {
+                        Console.WriteLine("Мало");
+                    }
+                    else if (a > num)
+                    {
+                        Console.WriteLine("Багато");
+                    }
+
+                } while (true);
+            }
+        }
+    }
+}
