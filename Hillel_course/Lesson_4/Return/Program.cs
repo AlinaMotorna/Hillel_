5dfbdfcb35ed5477c761ddeb5b70d424178ce452
User <User@DESKTOP-CHKLJ5S>
2023-09-10 22:36:55 +0300

    new leasson

--- Hillel_course/Lesson_4/Return/Program.cs
+++ Hillel_course/Lesson_4/Return/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Return
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            bool prost = true;
+            Console.WriteLine("Введіть число\n");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    prost = false;
+                    break;
+                }
+            }
+            if (prost)
+            {
+                Console.WriteLine("Число просте");
+            }
+            else
+            {
+                Console.WriteLine("Число не просте");
+            }
+            Console.ReadKey();
+        }
+    }
+}
