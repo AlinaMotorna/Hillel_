91efd971e11bd3b95a32777faa18be560ccc9829
User <User@DESKTOP-CHKLJ5S>
2023-09-10 20:40:19 +0300

    new leasson

---
+++ Hillel_course/Lesson_4/Password/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            string password = "root", input = string.Empty;
+            while (input != password) 
+            {
+                Console.WriteLine("Введіть пароль:");
+                input = Console.ReadLine();
+                Console.WriteLine("Пароль не вірний!\n");
+            }
+            Console.WriteLine("Пароль вірний!");
+            Console.ReadKey();
+        }
+    }
+}
