4890de5dfec083e9c7ac2b6f5cb6b112d6ec32c9
User <User@DESKTOP-CHKLJ5S>
2023-09-12 01:19:35 +0300

    т

---
+++ Hillel_course/Lesson_5/Array_at_the_beginning/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Array_at_the_beginning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //До масиву додається елемент на початок.
+            int[] nums = { -100, 2, 3, 4, 5, 6, 70, 8, 9, 10 };
+            int a = 1000;
+            int[] largerArray = new int[nums.Length + 1];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                largerArray[i] = nums[i];
+            }
+            largerArray[largerArray.Length - 1] = a;
+
+            foreach (var elem in largerArray)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
+
