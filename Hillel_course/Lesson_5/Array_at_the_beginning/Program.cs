4f4070dc8bb89e76943eacbe4b426c5ce9da30f4
User <User@DESKTOP-CHKLJ5S>
2023-09-12 02:07:03 +0300

    new

--- Hillel_course/Lesson_5/Array_at_the_beginning/Program.cs
+++ Hillel_course/Lesson_5/Array_at_the_beginning/Program.cs
@@ -5,16 +5,17 @@
         static void Main(string[] args)
         {
             //До масиву додається елемент на початок.
-            int[] nums = { -100, 2, 3, 4, 5, 6, 70, 8, 9, 10 };
-            int a = 1000;
+            int[] nums = { 0, 3, 6, 8, 6, 7, 99, 50, 61, 10 };
+            int a = 10;
             int[] largerArray = new int[nums.Length + 1];
             for (int i = 0; i < nums.Length; i++)
             {
                 largerArray[i] = nums[i];
             }
-            largerArray[largerArray.Length - 1] = a;
+            largerArray[largerArray.Length - 11] = a;
 
             foreach (var elem in largerArray)
+                
             {
                 Console.WriteLine(elem);
             }
