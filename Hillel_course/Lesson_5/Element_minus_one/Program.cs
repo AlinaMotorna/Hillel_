0032aaccaee9b12cfdd9acf7ba0863dcaf65c669
User <User@DESKTOP-CHKLJ5S>
2023-09-12 17:08:40 +0300

    т

---
+++ Hillel_course/Lesson_5/Element_minus_one/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Element_minus_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Написати програму, яка виводить всі елементи масиву доки не зустрінеться елемент -1.
+
+        }
+    }
+}
+
