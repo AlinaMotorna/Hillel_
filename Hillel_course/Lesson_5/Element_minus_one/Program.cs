6ae667f3b8fdced79e08e86c4b07a3b84fb7d970
User <User@DESKTOP-CHKLJ5S>
2023-09-13 15:55:19 +0300

    new

--- Hillel_course/Lesson_5/Element_minus_one/Program.cs
+++ Hillel_course/Lesson_5/Element_minus_one/Program.cs
@@ -5,7 +5,18 @@
         static void Main(string[] args)
         {
             // Написати програму, яка виводить всі елементи масиву доки не зустрінеться елемент -1.
-
+            int stop = -1;
+            int[] numbers = { 26, 1, 7, 15, -1, 3, 3, 4, 5, 7 };
+            List<int> results = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == stop) break;
+                results.Add(numbers[i]);
+            }
+            results.Sort();
+            Console.WriteLine(string.Join("\n", results));
+            Console.ReadLine();
+           
         }
     }
 }
