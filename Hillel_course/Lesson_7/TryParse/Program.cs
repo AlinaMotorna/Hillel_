9f6d9615acc1bd9ac49bbb0231ed8bfeb1758d3e
User <User@DESKTOP-CHKLJ5S>
2023-09-20 19:03:29 +0300

    new

---
+++ Hillel_course/Lesson_7/TryParse/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Доробити калькулятор - використати TryParse.
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            double oper1, oper2, result;
+            Operation operation;
+
+            Console.Write("Введіть перший операнд: ");
+            oper1 = GetDoubleNumber();
+            Console.Write("Введіть другий операнд: ");
+            oper2 = GetDoubleNumber();
+            Console.Write("Введіть операцію 1- +, 2 - -, 3 - *, 4 - /   ");
+            operation = GetOperation();
+            result = DoOperation(oper1, oper2, operation);
+            Console.WriteLine("{0} {1} {2} = {3}", oper1, operation, oper2, result);
+        }
+
+        static Operation GetOperation()
+        {
+            Operation operation;
+            bool resParse = false;
+            do
+            {
+                string input = Console.ReadLine();
+                
+                if (string.IsNullOrEmpty(input))
+
+                {
+                    input = "5";
+                }
+                operation = (Operation)System.Enum.TryParse(typeof(Operation), input);
+
+
+            }
+            while ((int)operation < (int)Operation.Add || (int)operation > (int)Operation.Divide || (int)operation == (int)Operation.None);
+            return operation;
+        }
+
+        static double GetDoubleNumber()
+        {
+            double number;
+            bool resParse = false;
+            do
+            {
+                string input = Console.ReadLine();
+                resParse = double.TryParse(input, out number);
+            }
+            while (!resParse);
+            return number;
+        }
+
+        static double DoOperation(double oper1, double oper2, Operation operation)
+        {
+            double result;
+            switch (operation)
+            {
+                case Operation.Add:
+                    result = oper1 + oper2;
+                    break;
+                case Operation.Subtract:
+                    result = oper1 - oper2;
+                    break;
+                case Operation.Multiply:
+                    result = oper1 * oper2;
+                    break;
+                case Operation.Divide:
+                    result = oper1 / oper2;
+                    break;
+                default: throw new ArgumentException();
+            }
+            return result;
+        }
+    }
+}
+
