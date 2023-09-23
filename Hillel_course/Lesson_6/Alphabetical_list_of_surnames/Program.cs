0e33efabdb5dcebb2ce7f8d4935eef37b87aa7fe
User <User@DESKTOP-CHKLJ5S>
2023-09-23 19:51:51 +0300

    new

---
+++ Hillel_course/Lesson_6/Alphabetical_list_of_surnames/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Alphabetical_list_of_surnames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Відсортуйте введений список прізвищ за алфавітом та виведіть його на екран.
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            string[] strings = { "Примушко", "Моторна", "Зміївський","Ковтун" };
+            Array.Sort(strings);
+            PrintArray(strings);
+        }
+
+        private static void PrintArray(string[] strings)
+        {
+            Console.WriteLine(string.Join("\n", strings));
+            Console.ReadKey();
+        }
+
+
+    }
+}
