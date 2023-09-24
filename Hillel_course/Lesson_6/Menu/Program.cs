6f05d80f401f4e8d904ce098f27356a262adf33c
User <User@DESKTOP-CHKLJ5S>
2023-09-24 16:58:58 +0300

    new

--- Hillel_course/Lesson_6/Menu/Program.cs
+++ Hillel_course/Lesson_6/Menu/Program.cs
@@ -16,7 +16,7 @@
         Subject[1] = new string[] { "Фізкультура", "12", "10", "12", "12", "9", "9", "", "", "", "", "" };
         Subject[2] = new string[] { "Математика","2", "3", "3", "6", "4", "4", "", "", "", "", "" };
         Subject[3] = new string[] { "Фізика", "6", "8", "8", "9", "10", "6", "", "", "", "", "" };
-        Subject[4] = new string[] {"Хімія", "4", "3", "3", "6", "5", "6", "", "", "", "", "" };
+        Subject[4] = new string[] { "Хімія", "4", "3", "3", "6", "5", "6", "", "", "", "", "" };
         while (true)
         {
             string userInput = null;
@@ -31,14 +31,78 @@
             if(userInput == "Біологія")
             {
                 Console.Clear();
-                for (int i = 7 i< Subject[0].Length; i++)
+                for (int i = 7; i< Subject[0].Length; i++)
                 {
                     Console.Clear();
                     Console.Write($"Input new mark here {i} / {Subject[0].Length - 1}:");
                     Subject[0][i] = Console.ReadLine();
                 }
             }
+            if (userInput == "Фізкультура")
+            {
+                Console.Clear();
+                for (int i = 7; i < Subject[1].Length; i++)
+                {
+                    Console.Clear();
+                    Console.Write($"Input new mark here {i} / {Subject[1].Length - 1}:");
+                    Subject[1][i] = Console.ReadLine();
+                }
+            }
+            if (userInput == "Математика")
+            {
+                Console.Clear();
+                for (int i = 7; i < Subject[2].Length; i++)
+                {
+                    Console.Clear();
+                    Console.Write($"Input new mark here {i} / {Subject[2].Length - 1}:");
+                    Subject[2][i] = Console.ReadLine();
+                }
+            }
+            if (userInput == "Фізика")
+            {
+                Console.Clear();
+                for (int i = 7; i < Subject[3].Length; i++)
+                {
+                    Console.Clear();
+                    Console.Write($"Input new mark here {i} / {Subject[3].Length - 1}:");
+                    Subject[3][i] = Console.ReadLine();
+                }
+            }
+            if (userInput == "Хімія")
+            {
+                Console.Clear();
+                for (int i = 7; i < Subject[4].Length; i++)
+                {
+                    Console.Clear();
+                    Console.Write($"Input new mark here {i} / {Subject[4].Length - 1}:");
+                    Subject[4][i] = Console.ReadLine();
+                }
+            }
+            if ((Menu)input == Menu.About)
+            {
+                Console.Clear();
+                Console.WriteLine("Тут додаються оцінки");
+            }
+            if ((Menu)input == Menu.PrintArray)
+            if ((Menu)input == Menu.Exit)
+            {
+                    break;
+            }
+            if ((Menu)input == Menu.AvarageGrade)
+            {
+                int[] avarageNum = CalculateTheAvarage(Subject);
+                Console.Clear();
+                for (int i = 0;i < avarageNum.Length; i++)
+                {
+                    Console.WriteLine($"{Subject[i][0]} mark is{avarageNum[i]}");
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+
         }
+
+
     }
 
 }
