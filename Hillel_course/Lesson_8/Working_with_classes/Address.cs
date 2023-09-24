3379a02d2278ca101c4ce48affe6e55a85627082
User <User@DESKTOP-CHKLJ5S>
2023-09-24 19:38:41 +0300

    new

---
+++ Hillel_course/Lesson_8/Working_with_classes/Address.cs
@@ -0,0 +1,72 @@
+﻿namespace Working_with_classes
+{
+    internal class Address
+    {
+        
+		private int index;
+
+		public int Index
+		{
+			get { return index; }
+			set { 
+				if (value >= 0000 && value <= 9999) 
+				index = value; 
+			}
+		}
+        private string country;
+
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                country = value;
+            }
+        }
+        private string city;
+
+        public string City
+        {
+            get { return city; }
+            set
+            {
+                city = value;
+            }
+        }
+        private string street;
+
+        public string Street
+        {
+            get { return street; }
+            set
+            {
+                street = value;
+            }
+        }
+        private string house;
+
+        public string House
+        {
+            get { return house; }
+            set
+            {
+                house = value;
+            }
+        }
+        private string apartment;
+
+        public string Apartment
+        {
+            get { return apartment; }
+            set
+            {
+                apartment = value;
+            }
+        }
+        public void PrintAddress()
+        {
+            Console.WriteLine(Index + "\n" + Country+"\n"+City+"\n"+ Street+"\n"+House+"\n"+ Apartment);
+        }
+    }
+
+}
---
+++ Hillel_course/Lesson_8/Working_with_classes/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Working_with_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var address = new Address()
+            { Index = 6678, Country = "Ukraine",City = "Cherkassy",Street= "Gogol Street",House = "House No 2", Apartment = "Apartment-B" };
+            address.PrintAddress();
+            Console.ReadKey();  
+        }
+    }
+}
