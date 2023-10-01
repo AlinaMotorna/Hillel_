8a5b0e6931cb8bd1f7a86afdb31f33016d0f352e
User <User@DESKTOP-CHKLJ5S>
2023-10-01 20:33:00 +0300

    new

---
+++ Hillel_course/Lesson_9/Working_with_class_constructors/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Working_with_class_constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                Console.Write("Довжина: ");
+                int side1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ширина: ");
+                int side2 = Convert.ToInt32(Console.ReadLine());
+
+                int area = side1 * side2;
+
+                Console.WriteLine("Площа прямокутника: {0}", area);
+                Console.ReadLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Hillel_course/Lesson_9/Working_with_class_constructors/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿namespace Working_with_class_constructors
+{
+    internal class Rectangle
+    {
+        int side1;
+        int side2;
+
+        public int Side1 {get;}
+        public int Side2 { get; }
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = Side1;
+            this.side2 = Side2;
+        }
+       
+    }
+}
