a689a4fc5b52e0ce6ae41041c9a309b245632a8e
User <User@DESKTOP-CHKLJ5S>
2023-10-01 21:06:07 +0300

    new

---
+++ Hillel_course/Lesson_9/Information_about_the_user/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Metrics;
+using System.IO;
+using System.Net;
+
+namespace Information_about_the_user
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User()
+            { Login = "Stars",Name= "Alina",Surname = "Motorna",};
+            user.PrintUser();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hillel_course/Lesson_9/Information_about_the_user/User.cs
@@ -0,0 +1,41 @@
+﻿namespace Information_about_the_user
+{
+    internal class User
+    {
+        public string login;
+        public string name;
+        public string surname;
+        private string date = "1.12.23";
+
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                surname = value;
+            }
+        }
+
+        internal void PrintUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
