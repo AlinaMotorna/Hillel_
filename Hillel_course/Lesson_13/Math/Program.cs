bdaa4f93571fa73981526d9994bf5f68ca492069
User <User@DESKTOP-CHKLJ5S>
2023-10-19 19:21:44 +0300

    new

---
+++ Hillel_course/Lesson_13/Math/Program.cs
@@ -0,0 +1,23 @@
+﻿using Math;
+
+namespace Information_about_the_user
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            try
+            {
+                static int Division(int x, int y) => x / 0;
+            }
+            catch
+            {
+                Console.WriteLine("Ділення на 0");
+            }
+            string? selection = Console.ReadLine();
+            double x, y = 5;
+        }
+    }
+}
+
