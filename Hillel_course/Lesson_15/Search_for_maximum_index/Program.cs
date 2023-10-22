7f21962fa2ba7cb4dddd06d4e0c6fe5acd4b9bd6
User <User@DESKTOP-CHKLJ5S>
2023-10-22 20:01:08 +0300

    new

---
+++ Hillel_course/Lesson_15/Search_for_maximum_index/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Search_for_maximum_index
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] cost = { 10, 25, 8, 45, 15, 30, 55, 5 };
+
+            int max = -1;
+            int index = -1;
+            
+            for (int i = 0; i < cost.Length; i++)
+                if (max <= cost[i])
+                {
+                    max = cost[i];
+                    index = i;
+                }
+            Console.WriteLine(index);
+            Console.ReadKey();
+        }
+
+    }
+}
+
