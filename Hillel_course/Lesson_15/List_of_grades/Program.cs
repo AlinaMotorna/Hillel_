516557215449d2c4e9e36e47fc16b10e67bbbc34
User <User@DESKTOP-CHKLJ5S>
2023-10-22 19:51:23 +0300

    new

--- Hillel_course/Lesson_15/List_of_grades/Program.cs
+++ Hillel_course/Lesson_15/List_of_grades/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace List_of_grades
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                int[] arr = new int[] { 85, 92, 78, 95, 88, 90 };
+
+            var lst = from a in arr where a > 89 select a;
+            int[] arr2 = lst.ToArray();
+
+            for (int i = 0; i < arr2.Length; i++)
+                Console.Write($"{arr2[i]}\t");
+            Console.ReadKey();
+            }
+        
+    }
+}
+
